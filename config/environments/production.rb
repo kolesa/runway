7336768dd779eddf699b4e3203074be9f3d44832
kolesa <kolesaak@gmail.com>
2017-04-23 23:05:46 +0300

    cable url action cable to heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,6 +84,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_cable.url = 'wss://runway-kolesa.herokuapp.comcable'
+  config.action_cable.url = 'wss://runway-kolesa.herokuapp.com/cable'
   config.action_cable.allowed_request_origins = [ 'https://runway-kolesa.herokuapp.com']
 end
